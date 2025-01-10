ecb5effc614d6dd50a81ab6d3890a9fc4ade42a4
139199228 <139199228@qq.com>
2025-01-10 17:45:14 +0800

    v7

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/out' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/out/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Compare_text/out' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Compare_text/out/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
